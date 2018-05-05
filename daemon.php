18c6b81b78751d40cdb76f67af27d537a7b587c3
GwtLabs <kris@biggames.pl>
2018-05-06 01:28:38 +0200

    Refactoring.

--- daemon.php
+++ daemon.php
@@ -1,178 +1,231 @@
-<?php 
+<?php
+
 error_reporting(E_ALL);
 include 'utils.php';
+
 $config_json = file_get_contents('config.json');
-$config = json_decode($config_json,true);
-$sleep_const = $config['daemon_interval_in_sec'];
-$allowed_connection_limit = $config['ban_when_n_connections'];
-$ban_time = $config['ban_time'];
-$black_list_conf = "/etc/nginx/blacklisted_ips.conf";
-$cf_ips_conf = "/etc/nginx/cloudflare_settings.conf";
+$config = json_decode($config_json, true);
 
-$i = 0;
-$c_c = 17280;
-while (1) {
-	$i++;
-	if ($c_c >= 17280) {
-		//Update cloduflare ips each 17280 iterations which is equal to 24h with 5s daemon interval
-		updateCloudFlareIps($cf_ips_conf);
-		$c_c = 0;
-	}
-	$c_c++;
-	$starttime = microtime(true);
-	echo "\nIteration:".$i;
+$daemon = new Daemon($config);
+$daemon->run();
 
-	$array = getArrayOfIpsSorted($config);
-	$count = count($array);
-	echo "\nDetected ".$count." users in last ".$config['interval_in_sec_considering_simultaneous_connections']."s";
-	$connections = 0;
-	foreach ($array as $key => $value) {
-		if ($value >= $allowed_connection_limit) {
-			save_log("Banning IP:".$key." for ".$value."/".$allowed_connection_limit." connections");
-			ban_ip($key,$ban_time,$black_list_conf);
-		}
-	}
-	checkBannedIpForUnbanOpportunity($black_list_conf);
+class Daemon
+{
+    private $sleep_const = 5;
+    private $allowed_connection_limit = 150;
+    private $black_list_conf = '/etc/nginx/blacklisted_ips.conf';
+    private $cf_ips_conf = '/etc/nginx/cloudflare_settings.conf';
+    private $add_points_per_baned_ip = 10;
+    private $sub_points_per_tick = 1;
+    private $i = 0;
+    private $c_c = 17280;
+    private $under_attack_pointer = 0;
+    private $config = [];
+    private $bannedIpList = [];
+    private $nowBanned = [];
 
-	$duration = microtime(true) - $starttime;
-	echo "\nExecution Time:".$duration.'s';
-	$sleep = abs($sleep_const - $duration);
-	echo "\nSleeping for:".$sleep;
-	usleep($sleep*1000000);
+    public function __construct($config)
+    {
+        $this->config = $config;
+        $this->sleep_const = $config['daemon_interval_in_sec'];
+        $this->allowed_connection_limit = $config['ban_when_n_connections'];
+        $this->add_points_per_baned_ip = 10;
+        $this->under_attack_pointer = 0;
+    }
+
+    public function run()
+    {
+        while (1) {
+            $this->i++;
+            if ($this->c_c >= 17280) {
+                //Update cloduflare ips each 17280 iterations which is equal to 24h with 5s daemon interval
+                $this->updateCloudFlareIps($this->cf_ips_conf);
+                $this->c_c = 0;
+            }
+            $this->c_c++;
+            $starttime = microtime(true);
+            echo "\n===============================";
+            echo "\nIteration:" . $this->i;
+            echo "\nUnder Attack Pointer: " . $this->under_attack_pointer;
+
+            $this->refreshBannedIpList();
+            $ips = getArrayOfIpsSorted($this->config);
+            echo "\nDetected " . count($ips) . ' users in last ' . $this->config['interval_in_sec_considering_simultaneous_connections'] . ' s.';
+            foreach ($ips as $ip => $connections) {
+                if ($connections >= $this->allowed_connection_limit) {
+                    $this->toBan($ip, $connections);
+                }
+            }
+            $this->saveBannedIpList();
+            $this->checkBannedIpForUnbanOpportunity();
+
+            if ($this->under_attack_pointer > 0) {
+                $this->under_attack_pointer -= $this->sub_points_per_tick;
+                $this->save_log('Under attack pointer reduced to: ' . $this->under_attack_pointer);
+            }
+            $duration = microtime(true) - $starttime;
+            echo "\nExecution Time: " . $duration . ' s.';
+            $sleep = abs($this->sleep_const - $duration);
+            echo "\nSleeping for:" . $sleep;
+            usleep($sleep * 1000000);
+        }
+    }
+
+    private function refreshBannedIpList()
+    {
+        $output = file_get_contents($this->black_list_conf);
+        $line_by_line = $this->remove_empty(explode("\n", $output));
+        echo "\nBanned ips: " . count($line_by_line);
+        $list = [];
+        foreach ($line_by_line as $line) {
+            if ($line) {
+                $value = explode('#', $line);
+                $banned_ip = $value[0];
+                $banned_ip = str_replace('deny ', '', $banned_ip);
+                $banned_ip = str_replace(';', '', $banned_ip);
+                $list[$banned_ip] = $value[1];
+            }
+        }
+        $this->bannedIpList = $list;
+    }
+
+    private function isAlredyBanned($ip)
+    {
+        return array_key_exists($ip, $this->bannedIpList);
+    }
+
+    private function getBanTime()
+    {
+        if ($this->under_attack_pointer <= $this->add_points_per_baned_ip * 2) {
+            return $this->config['ban_time_lo'];
+        } elseif ($this->under_attack_pointer > $this->add_points_per_baned_ip * 10) {
+            return $this->config['ban_time_hi'];
+        }
+        return $this->config['ban_time_md'];
+    }
+
+    private function checkBannedIpForUnbanOpportunity()
+    {
+        $new_file = '';
+        $removed = false;
+        $time = time();
+        foreach ($this->bannedIpList as $ip => $ubTime) {
+            if ($time > $ubTime) {
+                $this->save_log('Removing banned IP: ' . $ip);
+                $removed = true;
+            } else {
+                $new_file .= 'deny ' . $ip . ';#' . $ubTime . "\n";
+            }
+        }
+        if (!$removed) {
+            return;
+        }
+        file_put_contents($this->black_list_conf, $new_file);
+        $this->reloadNginxSettings();
+        echo "\nSaved new blacklist conf.";
+    }
+
+    private function addNewBan($ip, $time)
+    {
+        $this->under_attack_pointer += $this->add_points_per_baned_ip;
+        $this->save_log('Under attack pointer increased to: ' . $this->under_attack_pointer);
+        $this->nowBanned[$ip] = $time;
+    }
+
+    private function toBan($ip, $connections)
+    {
+        if ($this->isAlredyBanned($ip)) {
+            $this->save_log('IP ' . $ip . ' has been already banned.');
+            return;
+        }
+        $this->save_log('Banning IP: ' . $ip . ' for ' . $connections . '/' . $this->allowed_connection_limit . ' connections.');
+        $this->addNewBan($ip, $this->getBanTime());
+    }
+
+    private function saveBannedIpList()
+    {
+        if (!$this->nowBanned) {
+            return;
+        }
+        $now = time();
+        foreach ($this->nowBanned as $ip => $time) {
+            $unbanTime = $now + $time;
+            $this->bannedIpList[$ip] = $unbanTime;
+            $content = 'deny ' . $ip . ';#' . $unbanTime . "\n";
+            file_put_contents($this->black_list_conf, $content, FILE_APPEND);
+            $this->save_log('IP: ' . $ip . ' has been banned for ' . $time . ' s.');
+        }
+        $this->nowBanned = [];
+        $this->reloadNginxSettings();
+    }
+
+    private function updateCloudFlareIps($cf_ips_conf)
+    {
+        echo "\nUpdating CF ips";
+        $ipv4_list = $this->getForURL('https://www.cloudflare.com/ips-v4');
+        $ipv6_list = $this->getForURL('https://www.cloudflare.com/ips-v6');
+        $line_by_line_ipv4 = $this->remove_empty(explode("\n", $ipv4_list));
+        $line_by_line_ipv6 = $this->remove_empty(explode("\n", $ipv6_list));
+        $real_cf_ip_list = [];
+        foreach ($line_by_line_ipv4 as $key => $value) {
+            array_push($real_cf_ip_list, 'set_real_ip_from ' . $value . ';');
+        }
+        foreach ($line_by_line_ipv6 as $key => $value) {
+            array_push($real_cf_ip_list, 'set_real_ip_from ' . $value . ';');
+        }
+        if (!$real_cf_ip_list) {
+            $this->save_log('[Warning] CF ips is empty! Canceled.');
+            return;
+        }
+        array_push($real_cf_ip_list, 'real_ip_header CF-Connecting-IP;');
+        echo "\nNew CF ips acquired";
+        var_dump($real_cf_ip_list);
+        echo "\nSaving now";
+        $new_file = '';
+        foreach ($real_cf_ip_list as $key => $value) {
+            $new_file = $new_file . "\n" . $value;
+        }
+        file_put_contents($cf_ips_conf, $new_file);
+        $this->reloadNginxSettings();
+        echo 'New CF ips saved and applied! - Next check in ~24h';
+    }
+
+    private function getForURL($url)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => 'DDOS-DEFLATE for Cloudflare & Nginx [v1.0]'
+        ));
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        return $resp;
+    }
+
+    private function remove_empty($array)
+    {
+        return array_filter($array, [$this, '_remove_empty_values']);
+    }
+
+    private function _remove_empty_values($value)
+    {
+        return !empty($value) || $value === 0;
+    }
+
+    private function reloadNginxSettings()
+    {
+        $output = shell_exec('/etc/init.d/nginx reload');
+        echo "\n" . $output;
+    }
+
+    private function save_log($msg)
+    {
+        $now = time();
+        $date = date('Y-m-d H:i:s', $now);
+        $msg = '[' . $date . '] - ' . $msg . "\n";
+        $file = 'log/daemon-' . date('Y-m-d', $now) . '.log';
+        file_put_contents($file, $msg, FILE_APPEND);
+        echo $msg;
+    }
 }
-
-function checkBannedIpForUnbanOpportunity($black_list_conf){
-	$output = file_get_contents($black_list_conf);
-	$line_by_line = remove_empty(explode("\n", $output));
-	$isIpBannedAlready = 0;
-	echo "\nListing all banned ips:";
-	$rewrited_array = array();
-	foreach ($line_by_line as $key => $value) {
-		if ($value) {
-			$value_array = explode('#', $value);
-			$banned_ip = $value_array[0];
-			$banned_ip = str_replace('deny ', '', $banned_ip);
-			$banned_ip = str_replace(';', '', $banned_ip);
-			$unban_timestamp = $value_array[1];
-			$current_timestamp = time();
-			$diff = $unban_timestamp - $current_timestamp;
-			echo "\n".$value." connections from ".$key." left ".$diff.'s ban';
-			if ($diff < 0) {
-				save_log("Removing banned IP:".$banned_ip);
-			} else {
-				array_push($rewrited_array, $value);
-			}
-		}
-	}
-	if (!array_equal($rewrited_array,$line_by_line)) {
-		$new_file = '';
-		foreach ($rewrited_array as $key => $value) {
-			$new_file = $new_file."\n".$value;
-		}
-		file_put_contents($black_list_conf, $new_file);
-		reloadNginxSettings();
-		echo "Saved new blacklist conf";
-	} else {
-		echo "\nArrays are the same, keeping old records";
-	}
-}
-
-function ban_ip($ip,$ban_time,$black_list_conf){
-	$output = file_get_contents($black_list_conf);
-	$line_by_line = explode("\n", $output);
-	$isIpBannedAlready = 0;
-	echo "\nListing all banned ips:";
-	foreach ($line_by_line as $key => $value) {
-		if ($value) {
-			$value_array = explode('#', $value);
-			$banned_ip = $value_array[0];
-			$banned_ip = str_replace('deny ', '', $banned_ip);
-			$banned_ip = str_replace(';', '', $banned_ip);
-			$unban_timestamp = $value_array[1];
-			$current_timestamp = time();
-			$difference_to_unban = $unban_timestamp - $current_timestamp;
-			echo "\nCurrently banned ip:".$banned_ip." unban will occur after:".$difference_to_unban."s";
-			if ($banned_ip == $ip) {
-				$isIpBannedAlready = 1;
-				save_log("This ip has been already banned");
-			}
-		}
-	}
-	if (!$isIpBannedAlready) {
-		$timestamp_c = time();
-		$unban_timestamp = $timestamp_c + $ban_time;
-		$content = "\ndeny ".$ip.";#".$unban_timestamp;
-		file_put_contents($black_list_conf, $content, FILE_APPEND);
-		reloadNginxSettings();
-		save_log("IP:".$ip." has been banned for ".$ban_time."s");
-	}
-}
-
-function updateCloudFlareIps($cf_ips_conf){
-	echo "\nUpdating CF ips";
-	$ipv4_list = getForURL('https://www.cloudflare.com/ips-v4');
-	$ipv6_list = getForURL('https://www.cloudflare.com/ips-v6');
-	$line_by_line_ipv4 = remove_empty(explode("\n", $ipv4_list));
-	$line_by_line_ipv6 = remove_empty(explode("\n", $ipv6_list));
-	$real_cf_ip_list = array();
-	foreach ($line_by_line_ipv4 as $key => $value) {
-		array_push($real_cf_ip_list, "set_real_ip_from ".$value.";");
-	}
-	foreach ($line_by_line_ipv6 as $key => $value) {
-		array_push($real_cf_ip_list, "set_real_ip_from ".$value.";");
-	}
-	array_push($real_cf_ip_list, "real_ip_header CF-Connecting-IP;");
-	echo "\nNew CF ips acquired";
-	var_dump($real_cf_ip_list);
-	echo "\nSaving now";
-	$new_file = '';
-	foreach ($real_cf_ip_list as $key => $value) {
-		$new_file = $new_file."\n".$value;
-	}
-	file_put_contents($cf_ips_conf, $new_file);
-	reloadNginxSettings();
-	echo "New CF ips saved and applied! - Next check in ~24h";
-}
-
-function getForURL($url){
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-    	CURLOPT_RETURNTRANSFER => 1,
-    	CURLOPT_URL => $url,
-    	CURLOPT_USERAGENT => 'DDOS-DEFLATE for Cloudflare & Nginx [v1.0]'
-	));
-	$resp = curl_exec($curl);
-	curl_close($curl);
-	return $resp;
-}
-
-function array_equal($a, $b) {
-    return (
-         is_array($a) && is_array($b) && 
-         count($a) == count($b) &&
-         array_diff($a, $b) === array_diff($b, $a)
-    );
-}
-
-function remove_empty($array) {
-  return array_filter($array, '_remove_empty_values');
-}
-
-function _remove_empty_values($value) {
-  return !empty($value) || $value === 0;
-}
-
-function reloadNginxSettings(){
-	$output = shell_exec('/etc/init.d/nginx reload');
-	echo "\n".$output;
-}
-
-function save_log($msg){
-	$date = date('m/d/Y h:i:s a', time());
-    $msg = "\n[".$date."] - ".$msg;
-    $file = 'daemon_logs.txt';
-    file_put_contents($file, $msg, FILE_APPEND);
-    echo $msg;
- }
-
-?>
--- utils.php
+++ utils.php
@@ -1,36 +1,35 @@
-<?php 
+<?php
 error_reporting(0);
 
-function getArrayOfIpsSorted($config){
-	$n_lines_analyse = $config['lines_to_analyse'];
-	$nginx_log_path = $config['nginx_log_path'];
-	$interval = $config['interval_in_sec_considering_simultaneous_connections'];
-	$output = shell_exec('tail -'.$n_lines_analyse.' '.$nginx_log_path);
-	$line_by_line = explode("\n", $output);
-	$outputarray = array();
-	foreach ($line_by_line as $key => $value) {
-		preg_match('/^(\S+) (\S+) (\S+) \[([^:]+):(\d+:\d+:\d+) ([^\]]+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) (\".*?\") (\".*?\")$/',$value, $matches);
-		if ($matches) {
-			$date = $matches[4];
-			$time = $matches[5];
-			$ip = $matches[1];
-			$date_time = $date." ".$time;
-			$datetime = DateTime::createFromFormat($config['nginx_dateformat'], $date_time);
-			$timestamp = $datetime->getTimestamp();
-			$current_timestamp = time();
-			$difference = $current_timestamp - $timestamp;
-			if ($difference < $interval) {
-				if (!$outputarray[$ip]) {
-					$outputarray[$ip] = 1;
-				} else {
-					$outputarray[$ip] = $outputarray[$ip] + 1;
-				}
-			}
-			//echo "\nTimestamp:".$timestamp." IP:".$ip." Date:".$date_time;
-		}
-	}
-	asort($outputarray);
-	return array_reverse($outputarray);
+function getArrayOfIpsSorted($config)
+{
+    $n_lines_analyse = $config['lines_to_analyse'];
+    $nginx_log_path = $config['nginx_log_path'];
+    $interval = $config['interval_in_sec_considering_simultaneous_connections'];
+    $output = shell_exec('tail -' . $n_lines_analyse . ' ' . $nginx_log_path);
+    $line_by_line = array_reverse(explode("\n", $output));
+    $outputarray = [];
+    $current_timestamp = time();
+    foreach ($line_by_line as $key => $value) {
+        preg_match('/^(\S+) (\S+) (\S+) \[([^:]+):(\d+:\d+:\d+) ([^\]]+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) (\".*?\") (\".*?\")$/', $value, $matches);
+        if ($matches) {
+            $date = $matches[4];
+            $time = $matches[5];
+            $ip = $matches[1];
+            $date_time = $date . " " . $time;
+            $datetime = DateTime::createFromFormat($config['nginx_dateformat'], $date_time);
+            if ($current_timestamp - $datetime->getTimestamp() < $interval) {
+                if (!$outputarray[$ip]) {
+                    $outputarray[$ip] = 1;
+                } else {
+                    $outputarray[$ip] = $outputarray[$ip] + 1;
+                }
+                continue;
+            }
+            break;
+            //echo "\nTimestamp:".$timestamp." IP:".$ip." Date:".$date_time;
+        }
+    }
+    asort($outputarray);
+    return array_reverse($outputarray);
 }
-
-?>
